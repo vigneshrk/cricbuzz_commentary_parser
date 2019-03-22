f25279288b78523dbe10a27814fba6515b55a913
vigneshrk <vigneshrk770@gmail.com>
2019-03-23 01:07:01 +0530

    added options

--- parse_commentary.py
+++ parse_commentary.py
@@ -2,29 +2,34 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-import re, os
-import configparser
+import sys, re, os, configparser, argparse, json
 
+driver = None
 
-chrome_driver_path = "./resources/chromedriver/chromedriver_linux"
-options = Options()
-config = configparser.ConfigParser()
-if 'chrome_binary_location' in config['DEFAULT']:
-    options.binary_location = config['DEFAULT']['chrome_binary_location']
-    print('using custom chrome from '+options.binary_location)
-else:
-    print('using default chrome')
-driver = webdriver.Chrome(options=options, executable_path=chrome_driver_path)
+def init_driver():
+    global driver
+    chrome_driver_path = "./resources/chromedriver/chromedriver_linux"
+    options = Options()
+    config = configparser.ConfigParser()
+    if 'chrome_binary_location' in config['DEFAULT']:
+        options.binary_location = config['DEFAULT']['chrome_binary_location']
+        print('using custom chrome from '+options.binary_location)
+    else:
+        print('using default chrome')
+    driver = webdriver.Chrome(options=options, executable_path=chrome_driver_path)
 
 def open_url(url):
+    global driver
     driver.get(url)
     sleep(1)
 
 def close_driver():
+    global driver
     driver.close()
     driver.quit()
 
-def scrap_data():
+def scrap_and_dump_data(filename):
+    global driver
     teams = []
     inns_btns = {}
     for elem in driver.find_elements_by_class_name("cb-nav-pill-1"):
@@ -62,11 +67,28 @@ def scrap_data():
 
 
     print(scrapped_data)
+    with open(filename, "w") as text_file:
+        text_file.write(json.dumps(scrapped_data))
 
 
 
 if __name__ == '__main__':
-    url = "https://www.cricbuzz.com/live-cricket-full-commentary/21538/ind-vs-aus-2nd-t20i-australia-tour-of-india-2019"
-    open_url(url)
-    scrap_data()
-    close_driver()
+    parser = argparse.ArgumentParser(usage="test", description="test desc")
+    parser.add_argument("-u", "--url", help="full cricbuzz commentary URL of a cricket match. Example - https://www.cricbuzz.com/live-cricket-full-commentary/21538/ind-vs-aus-2nd-t20i-australia-tour-of-india-2019")
+    parser.add_argument("-f", "--file", help="file to dump the data")
+    args = parser.parse_args()
+    # url = "https://www.cricbuzz.com/live-cricket-full-commentary/21538/ind-vs-aus-2nd-t20i-australia-tour-of-india-2019"
+    # outputfile = "/home/vignesh/proj/dev/data/result.json"
+    url = args.url
+    print(url)
+    if not args.file:
+        print("Please enter a filename to dump data")
+        sys.exit(0)
+    if url:
+        init_driver()
+        open_url(url)
+        scrap_and_dump_data(args.file)
+        close_driver()
+    else:
+        print("Please enter a full commentary url to parse")
+        sys.exit(0)
